0446a62802899b214aad48a3f0e3d6535dbc4b1c
Thomas Guillet <guillet.thomas@gmail.com>
2016-11-04 17:17:27 +0100

    [Bokeh] Fix axes to (0,1)

--- random_point.py
+++ random_point.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure
 from bokeh.layouts import layout
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Range1d
 from bokeh.models.widgets import Button
 from bokeh.io import curdoc
 
@@ -9,6 +9,8 @@ import numpy as np
 source = ColumnDataSource(data=dict(x=[], y=[]))
 
 p = figure()
+unit_range = Range1d(0,1)
+p.x_range, p.y_range = unit_range, unit_range
 p.circle(x='x', y='y', source=source)
 
 def generate():
