8d2818f88bd9dc623c034cb68cb1a0a84a51bf93
Thomas Guillet <guillet.thomas@gmail.com>
2016-11-04 16:21:12 +0100

    [Bokeh] Fix point additions using ColumnDataSource

--- random_point.py
+++ random_point.py
@@ -1,14 +1,19 @@
 from bokeh.plotting import figure
 from bokeh.layouts import layout
+from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Button
 from bokeh.io import curdoc
 
 import numpy as np
 
+source = ColumnDataSource(data=dict(x=[], y=[]))
+
 p = figure()
+p.circle(x='x', y='y', source=source)
 
 def generate():
-	p.circle(*zip(np.random.random(size=2)))
+	values = [[v] for v in np.random.random(size=2)]
+	source.data = dict(x=values[0], y=values[1])
 
 generate_button = Button(label='Generate')
 generate_button.on_click(generate)
